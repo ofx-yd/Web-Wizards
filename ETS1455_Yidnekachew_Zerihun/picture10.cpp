3a35cc53daee8c98de695771e9fdb97bcdd265e0
ofx-yd <bossyid8@gmail.com>
2024-12-10 13:52:01 +0300

    Create picture10.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+  for(int i=1;i<=5;i++){
+      char a='A';
+      for(int k=5-i;k>0;k--)
+      cout<<" ";
+      for(int j=1;j<=i;j++){
+      cout<<" *";
+     
+      }
+  cout<<endl;
+  
+  }
+  return 0;
+}
