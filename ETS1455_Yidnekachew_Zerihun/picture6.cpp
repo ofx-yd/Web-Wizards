e550deab1b6d357bc17e590d57b5f87b34e7d14f
ofx-yd <bossyid8@gmail.com>
2024-12-10 13:48:44 +0300

    Create picture6.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+  for(int i=1;i<=5;i++){
+      char a='a';
+      for(int j=1;j<=5;j++){
+      cout<<a++<<" ";
+  }cout<<endl;
+  }
+
+  
+    EXIT_SUCCESS;
+}
