ec8d51570393cb97298f215e6d166dadfc2c418d
ofx-yd <bossyid8@gmail.com>
2024-12-10 13:44:55 +0300

    Create picture3.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int j=1;j<=5;j++){
+        for(int i=1;i<=j;i++){
+            cout<<i;
+        }cout<<endl;
+    }
+
+    EXIT_SUCCESS;
+}
