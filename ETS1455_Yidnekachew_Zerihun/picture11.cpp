4beadfc3104965ac673abd011c31a3bb4e0181e9
ofx-yd <bossyid8@gmail.com>
2024-12-10 13:54:59 +0300

    Create picture11.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int j=5;j>=1;j--){
+        for(int i=5-j;i>=1;i--){
+            cout<<" ";
+            }    
+            for(int k=j;k>=1;k--){
+            cout<<"* ";
+            }
+        cout<<endl;
+    }
+
+
+
+
+
+
+
+
+    EXIT_SUCCESS;
+}
