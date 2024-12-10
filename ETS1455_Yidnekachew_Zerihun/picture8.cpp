fe9fb70f1c054f7d7463776eb903ef36a638e11d
ofx-yd <bossyid8@gmail.com>
2024-12-10 13:51:04 +0300

    Create picture8.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+  for(int i=1;i<=7;i++){
+      for(int k=7-i;k>0;k--)
+      cout<<"*";
+      
+  cout<<endl;
+  
+  }
+  return 0;
+}
+    
