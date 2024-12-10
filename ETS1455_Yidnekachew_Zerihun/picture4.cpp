b43c3e66d79ea49c80d6c1d5d6b879ba3edde76d
ofx-yd <bossyid8@gmail.com>
2024-12-10 13:46:52 +0300

    Create picture4.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+  for(int i=1;i<=5;i++){
+      char a='A';
+      for(int k=5-i;k>0;k--)
+      cout<<" ";
+    for(int j=1;j<=i;j++){
+      cout<<"k";
+     
+      }
+      
+  cout<<endl;
+  
+  }
+  return 0;
+}
+    
