0733686ba1aa85b720a3b92af735064498cc2ccc
ofx-yd <bossyid8@gmail.com>
2024-12-10 13:50:28 +0300

    Create picture7.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  for(int i=1;i<=5;i++){
+      char a='a';
+      for(int j=1;j<=5;j++){
+         
+         if(j>=2&&j<=4&&i>=2&&i<=4){
+         cout<<"  ";
+      }else
+         cout<<"* ";
+  }cout<<endl;
+  }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    EXIT_SUCCESS;
+}
